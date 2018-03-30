d74e5dd55a2597963d00b1ac8772b9641264ca51
Christian Oder <myself5@carbonrom.org>
2018-03-30 22:24:46 +0200

    Revert "Refresh UI on force stop/disable"
    This reverts commit e5e91fa90543f11a1bc58a0b90134c820a8e4961.
    
    Change-Id: I098b248678e55a26843df16157534f648e7766a5

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -902,9 +902,6 @@ public class InstalledAppDetails extends AppInfoBase
 
     @VisibleForTesting
     void checkForceStop() {
-        if (getActivity() == null || getActivity().isFinishing()) {
-            return;
-        }
         if (mDpm.packageHasActiveAdmins(mPackageInfo.packageName)) {
             // User can't force stop device admin.
             //Log.w(LOG_TAG, "User can't force stop device admin");
@@ -1414,13 +1411,6 @@ public class InstalledAppDetails extends AppInfoBase
             mPm.setApplicationEnabledSetting(mInfo.packageName, mState, 0);
             return null;
         }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            if (mActivity.get() !=  null) {
-                mActivity.get().refreshUi();
-            }
-        }
     }
 
     private final LoaderCallbacks<ChartData> mDataCallbacks = new LoaderCallbacks<ChartData>() {
@@ -1449,7 +1439,6 @@ public class InstalledAppDetails extends AppInfoBase
             //Log.d(LOG_TAG, "Got broadcast response: Restart status for "
             //        + mAppEntry.info.packageName + " " + enabled);
             updateForceStopButton(enabled);
-            refreshUi();
         }
     };
 
