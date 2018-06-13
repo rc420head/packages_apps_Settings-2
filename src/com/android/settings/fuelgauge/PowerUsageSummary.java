dbc41303de4c671d84d5fa38f9eb5f1cbaee2230
calebcabob <calphonic@gmail.com>
2018-06-12 20:37:53 -0400

    Revert "Alert when screen off and charging if charge level becomes good [2/2]"
    This reverts commit 2de5921246dc5d9c54e88a46e279cbdc62581b7f.
    
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -68,7 +68,6 @@ import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.AutoBrightnessPreferenceController;
 import com.android.settings.display.BatteryPercentagePreferenceController;
-import com.android.settings.display.BatteryChargeAlertPreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
@@ -350,7 +349,6 @@ public class PowerUsageSummary extends PowerUsageBase implements
         //        new AmbientDisplayConfiguration(context),
         //        KEY_AMBIENT_DISPLAY));
         controllers.add(new SmartPixelsPreferenceController(context));
-        controllers.add(new BatteryChargeAlertPreferenceController(context));
         return controllers;
     }
 
