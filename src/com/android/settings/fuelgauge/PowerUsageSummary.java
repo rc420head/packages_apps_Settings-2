97ee063b78255689ab35d12be6d94956a6f700e0
rc420head <dennislebhardt79@gmail.com>
2018-07-08 00:40:53 -0500

    this makes it caf branch

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -68,6 +68,7 @@ import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.display.AmbientDisplayPreferenceController;
 import com.android.settings.display.AutoBrightnessPreferenceController;
 import com.android.settings.display.BatteryPercentagePreferenceController;
+import com.android.settings.display.BatteryChargeAlertPreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
@@ -349,6 +350,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         //        new AmbientDisplayConfiguration(context),
         //        KEY_AMBIENT_DISPLAY));
         controllers.add(new SmartPixelsPreferenceController(context));
+        controllers.add(new BatteryChargeAlertPreferenceController(context));
         return controllers;
     }
 
